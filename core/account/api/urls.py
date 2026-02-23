f6387262ab6ee37024decda631b1c6cbc4eb23e2
Amir Hossein <amirh3347@gmail.com>
2026-02-24 00:07:09 +0330

    add refresh jwt token api and remove update last_login by simple jwt

--- core/account/api/urls.py
+++ core/account/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from account.api.views import RegisterViewSet, LoginViewSet
 
 
@@ -8,4 +10,5 @@ urlpatterns = [
 
     path('v1/register/', RegisterViewSet.as_view(), name='register'),
     path('v1/login/', LoginViewSet.as_view(), name='login'),
+    path('v1/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
